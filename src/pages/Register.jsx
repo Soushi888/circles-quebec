eb9770d5d27d1c7ca3fdf659a93205353d6a109d
Soushi888 <info@sacha-pignot.com>
2020-12-17 00:12:23 -0500

    Intégration compposante RegisterForm

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import RegisterForm from "../components/RegisterForm/RegisterForm";
 
 export default function Register() {
   return (
     <div>
       <h1>Formulaire d'enregistrement</h1>
+      <RegisterForm />
     </div>
   );
 }
