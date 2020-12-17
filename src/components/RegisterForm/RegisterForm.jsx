dc9e8ea543dc14e70fba0d372d155437f36eaf56
Soushi888 <info@sacha-pignot.com>
2020-12-17 00:28:01 -0500

    Création de la composante RegisterForm

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./RegisterForm.module.css";
+
+const RegisterForm = () => {
+  return <form className={style.RegisterForm}>
+      
+  </form>;
+};
+
+export default RegisterForm;
