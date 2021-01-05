3d74fbaf9f318af070f6172d71c326b53f0ca020
Soushi888 <Sacha.pignot@gmail.com>
2021-01-04 21:32:15 -0500

    Ajout modal

--- src/components/RepertoireTable/RepertoireTable.jsx
+++ src/components/RepertoireTable/RepertoireTable.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./RepertoireTable.module.css";
+import ReactModal from "react-modal";
 import JSONdata from "../../data/repertoire.json";
 
 const RepertoireTable = () => {
   const data = Object.values(JSONdata);
+
+  // Modal setup
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+  ReactModal.setAppElement(document.querySelector("#root"));
 
   return (
     <div className={style.RepertoireTable}>
@@ -19,13 +26,28 @@ const RepertoireTable = () => {
         </thead>
         <tbody>
           {data.map((d, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{d.entreprise}</td>
-              <td>{d.service}</td>
-              <td>{d.duree}</td>
-              <td>{d.cout} circles</td>
-            </tr>
+            <React.Fragment key={i}>
+              <tr>
+                <td>{i + 1}</td>
+                <td className={style.RepertoireTable__link} onClick={openModal}>
+                  {d.entreprise}
+                </td>
+                <td>{d.service}</td>
+                <td>{d.duree}</td>
+                <td>{d.cout} circles</td>
+              </tr>
+
+              <ReactModal
+                className={style.Modal}
+                overlayClassName={style.Modal__overlay}
+                isOpen={showModal}
+                onRequestClose={closeModal}
+              >
+                <h2>{d.entreprise}</h2>
+                <p>{d.service}</p>
+                <button onClick={closeModal}>Close</button>
+              </ReactModal>
+            </React.Fragment>
           ))}
         </tbody>
       </table>
--- src/pages/Repertoire.jsx
+++ src/pages/Repertoire.jsx
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../components/Header/Header";
 import RepertoireTable from "../components/RepertoireTable/RepertoireTable";
-import Modal from "../components/Modal/Modal";
 
 export default function Repertoire() {
-  const [showModal, setShowModal] = useState(false);
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
-
   return (
     <main>
       <Header>Repertoire des entreprises</Header>
-      <button onClick={openModal}>Ouvrir Modal</button>
       <RepertoireTable />
-      <Modal header="lorem ipsum" showModal={showModal} closeModal={closeModal}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam,
-        voluptas? Officia quas harum dolorum cum laudantium nulla. Velit dolor
-        assumenda impedit doloribus minima, veniam quam ullam harum maiores
-        repellat ad?
-      </Modal>
     </main>
   );
 }
